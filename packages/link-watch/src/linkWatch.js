d92285e9fb415fc647710a72958fe7f56343da45
Rudy Huynh <rudyhuynh@niometrics.com>
2020-03-22 22:18:52 +0800

    remove scope from common package

--- packages/link-watch/src/linkWatch.js
+++ packages/link-watch/src/linkWatch.js
@@ -2,12 +2,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 const rimraf = require("rimraf");
-const {
-  getGlobalSymlinks,
-  globalLinkPath,
-  getNodeModulePath,
-  isLinked
-} = require("@npm-link-watch/common");
+const { getGlobalSymlinks, globalLinkPath, getNodeModulePath, isLinked } = require("npm-link-watch-common");
 var validateNpmPackageName = require("validate-npm-package-name");
 const chokidar = require("chokidar");
 
@@ -19,91 +14,80 @@ console.info(`${name} v${version}`);
 const cwd = process.cwd();
 
 function main() {
-  const param = params[0] || "";
-  if (param.startsWith("./")) {
-    link(params.filter(param => param.startsWith("./")));
-  } else if (validateNpmPackageName(param).validForNewPackages) {
-    watch(
-      params.filter(param => validateNpmPackageName(param).validForNewPackages)
-    );
-  } else {
-    console.error(
-      "Invalid parameter. Expect either a relative path (started with './') or a package name"
-    );
-  }
+    const param = params[0] || "";
+    if (param.startsWith("./")) {
+        link(params.filter(param => param.startsWith("./")));
+    } else if (validateNpmPackageName(param).validForNewPackages) {
+        watch(params.filter(param => validateNpmPackageName(param).validForNewPackages));
+    } else {
+        console.error("Invalid parameter. Expect either a relative path (started with './') or a package name");
+    }
 }
 
 function link(dirPaths) {
-  console.info(`Start to link: ` + dirPaths.join(", "));
+    console.info(`Start to link: ` + dirPaths.join(", "));
 
-  const packageName = require(path.resolve(cwd, "package.json")).name;
+    const packageName = require(path.resolve(cwd, "package.json")).name;
 
-  //clear previous
-  const globalPackagePath = path.resolve(globalLinkPath, packageName);
-  rimraf.sync(globalPackagePath);
+    //clear previous
+    const globalPackagePath = path.resolve(globalLinkPath, packageName);
+    rimraf.sync(globalPackagePath);
 
-  dirPaths.forEach(dirPath => {
-    const targetPath = path.resolve(cwd, dirPath);
-    const symlinkPath = path.resolve(globalLinkPath, packageName, dirPath);
-    const dirOfSymlink = path.dirname(symlinkPath);
-    if (!fs.existsSync(dirOfSymlink))
-      fs.mkdirSync(dirOfSymlink, { recursive: true });
+    dirPaths.forEach(dirPath => {
+        const targetPath = path.resolve(cwd, dirPath);
+        const symlinkPath = path.resolve(globalLinkPath, packageName, dirPath);
+        const dirOfSymlink = path.dirname(symlinkPath);
+        if (!fs.existsSync(dirOfSymlink)) fs.mkdirSync(dirOfSymlink, { recursive: true });
 
-    fs.symlinkSync(targetPath, symlinkPath);
-  });
+        fs.symlinkSync(targetPath, symlinkPath);
+    });
 
-  console.info(
-    `Link Successfully! Now you can go to your project and run "npx npm-link-watch ${packageName}"`
-  );
+    console.info(`Link Successfully! Now you can go to your project and run "npx npm-link-watch ${packageName}"`);
 }
 
 function watch(packageNames) {
-  console.info(`Start to watch and sync from ${packageNames.join(", ")}`);
+    console.info(`Start to watch and sync from ${packageNames.join(", ")}`);
 
-  for (let packageName of packageNames) {
-    if (!isLinked(packageName)) {
-      console.error(`ERROR: package ${packageName} is not linked yet.`);
-      return;
+    for (let packageName of packageNames) {
+        if (!isLinked(packageName)) {
+            console.error(`ERROR: package ${packageName} is not linked yet.`);
+            return;
+        }
     }
-  }
 
-  packageNames.forEach(packageName => watchPerPackage(packageName));
+    packageNames.forEach(packageName => watchPerPackage(packageName));
 
-  console.info("Ctrl+C to exit\n");
+    console.info("Ctrl+C to exit\n");
 }
 
 function watchPerPackage(packageName) {
-  const watchedFiles = getGlobalSymlinks(packageName).map(file =>
-    fs.readlinkSync(file)
-  );
+    const watchedFiles = getGlobalSymlinks(packageName).map(file => fs.readlinkSync(file));
 
-  watchedFiles.forEach(file => {
-    backup(getNodeModulePath(file, packageName));
-  });
-
-  chokidar
-    .watch(watchedFiles)
-    .on("add", filePath => {
-      sync(filePath, getNodeModulePath(filePath, packageName));
-    })
-    .on("change", filePath => {
-      sync(filePath, getNodeModulePath(filePath, packageName));
-    })
-    .on("unlink", filePath => {
-      rimraf.sync(getNodeModulePath(filePath, packageName));
+    watchedFiles.forEach(file => {
+        backup(getNodeModulePath(file, packageName));
     });
+
+    chokidar
+        .watch(watchedFiles)
+        .on("add", filePath => {
+            sync(filePath, getNodeModulePath(filePath, packageName));
+        })
+        .on("change", filePath => {
+            sync(filePath, getNodeModulePath(filePath, packageName));
+        })
+        .on("unlink", filePath => {
+            rimraf.sync(getNodeModulePath(filePath, packageName));
+        });
 }
 
 function sync(source, nodeModulePath) {
-  fs.copySync(source, nodeModulePath);
-  console.info("Synced: " + source + " --> " + nodeModulePath);
+    fs.copySync(source, nodeModulePath);
+    console.info("Synced: " + source + " --> " + nodeModulePath);
 }
 
 function backup(nodeModulePath) {
-  const backupPath = nodeModulePath + ".npm-link-watch-backup";
-  fs.existsSync(nodeModulePath) &&
-    !fs.existsSync(backupPath) &&
-    fs.copySync(nodeModulePath, backupPath);
+    const backupPath = nodeModulePath + ".npm-link-watch-backup";
+    fs.existsSync(nodeModulePath) && !fs.existsSync(backupPath) && fs.copySync(nodeModulePath, backupPath);
 }
 
 main();
--- packages/unlink-watch/src/unlinkWatch.js
+++ packages/unlink-watch/src/unlinkWatch.js
@@ -2,11 +2,7 @@
 const fs = require("fs-extra");
 const path = require("path");
 const rimraf = require("rimraf");
-const {
-  getGlobalSymlinks,
-  globalLinkPath,
-  getNodeModulePath
-} = require("@npm-link-watch/common");
+const { getGlobalSymlinks, globalLinkPath, getNodeModulePath } = require("npm-link-watch-common");
 
 const [, , ...packageNames] = process.argv;
 
@@ -16,28 +12,26 @@ console.info(`${name} v${version}`);
 main();
 
 function main() {
-  console.info(`Start to unlink ${packageNames.join(", ")}`);
+    console.info(`Start to unlink ${packageNames.join(", ")}`);
 
-  packageNames.forEach(packageName => unlinkPerPackage(packageName));
+    packageNames.forEach(packageName => unlinkPerPackage(packageName));
 
-  console.info("Unlink Successfully!");
+    console.info("Unlink Successfully!");
 }
 
 function unlinkPerPackage(packageName) {
-  const restoredFiles = getGlobalSymlinks(packageName).map(file =>
-    fs.readlinkSync(file)
-  );
+    const restoredFiles = getGlobalSymlinks(packageName).map(file => fs.readlinkSync(file));
 
-  // remove global
-  const removedPath = path.resolve(globalLinkPath, packageName);
-  rimraf.sync(removedPath);
-  console.info("Removed " + removedPath);
+    // remove global
+    const removedPath = path.resolve(globalLinkPath, packageName);
+    rimraf.sync(removedPath);
+    console.info("Removed " + removedPath);
 
-  // restore in node_modules/package-name
-  restoredFiles.forEach(file => {
-    const nodeModulePath = getNodeModulePath(file, packageName);
-    const backupPath = nodeModulePath + ".npm-link-watch-backup";
-    fs.existsSync(backupPath) && fs.copySync(backupPath, nodeModulePath);
-    console.info(" Restored: " + nodeModulePath);
-  });
+    // restore in node_modules/package-name
+    restoredFiles.forEach(file => {
+        const nodeModulePath = getNodeModulePath(file, packageName);
+        const backupPath = nodeModulePath + ".npm-link-watch-backup";
+        fs.existsSync(backupPath) && fs.copySync(backupPath, nodeModulePath);
+        console.info(" Restored: " + nodeModulePath);
+    });
 }
